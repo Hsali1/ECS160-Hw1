d1e69ff9e10cd3002dc3a53a953a85be5b368d13
Hassan Ali <Hassanalisid1996@gmail.com>
2025-01-19 23:50:26 -0800

    Initial setup

--- src/main/java/com/ecs160/hw1/SocialMediaAnalyzerDriver.java
+++ src/main/java/com/ecs160/hw1/SocialMediaAnalyzerDriver.java
@@ -5,10 +5,32 @@ package com.ecs160.hw1;
 import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
 import java.util.List;
+import com.google.gson.JsonDeserializer;
+import java.io.InputStreamReader;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+
 
 
 public class SocialMediaAnalyzerDriver {
     public static void main(String[] args) {
 
+
+        JsonElement element = JsonParser.parseReader(new InputStreamReader(JsonDeserializer.class.getClassLoader().getResourceAsStream("input.json")));
+        System.out.println("it work!");
+//        System.out.println(element); prints the entire file i think
+        if (element.isJsonObject()) {
+            JsonObject jsonObject = element.getAsJsonObject();
+            System.out.println("it work again!");
+            JsonArray feedArray = jsonObject.get("feed").getAsJsonArray();
+            for (JsonElement feedObject: feedArray) {
+                // Check if you have the thread key
+                if (feedObject.getAsJsonObject().has("thread")) {
+                    // parse the post and any replies (recursively)?
+                }
+            }
+        }
     }
 }
