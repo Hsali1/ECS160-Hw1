875fe366aca2d42aa88465d4b5a0f3a74a5cff7e
Hassan Ali <Hassanalisid1996@gmail.com>
2025-01-20 01:29:17 -0800

    set up redis

--- src/main/java/com/ecs160/hw1/SocialMediaAnalyzerDriver.java
+++ src/main/java/com/ecs160/hw1/SocialMediaAnalyzerDriver.java
@@ -11,26 +11,52 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonArray;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
 
 
 
 public class SocialMediaAnalyzerDriver {
-    public static void main(String[] args) {
+    private static final Logger log = LoggerFactory.getLogger(SocialMediaAnalyzerDriver.class);
 
-
-        JsonElement element = JsonParser.parseReader(new InputStreamReader(JsonDeserializer.class.getClassLoader().getResourceAsStream("input.json")));
-        System.out.println("it work!");
-//        System.out.println(element); prints the entire file i think
+    public static void parseDataIntoDatabase(String jsonFile, Jedis jedis){
+        JsonElement element = JsonParser.parseReader(new InputStreamReader(JsonDeserializer.class.getClassLoader().getResourceAsStream(jsonFile)));
+//        System.out.println("it work!");
+//        System.out.println(element); prints the entire file I think
         if (element.isJsonObject()) {
             JsonObject jsonObject = element.getAsJsonObject();
-            System.out.println("it work again!");
+//            System.out.println("it work again!");
             JsonArray feedArray = jsonObject.get("feed").getAsJsonArray();
             for (JsonElement feedObject: feedArray) {
                 // Check if you have the thread key
                 if (feedObject.getAsJsonObject().has("thread")) {
                     // parse the post and any replies (recursively)?
+                    // prints the entire thread key
+//                    System.out.println(feedObject.getAsJsonObject());
                 }
             }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try (Jedis jedis = new Jedis("localhost", 6379)) {
+
+            System.out.println("Connected to Redis!");
+
+            String inputFile = "input.json"; // Default file
+            if (args.length > 0) {
+                inputFile = args[0];
+            }
+
+            // Store json data into database
+            parseDataIntoDatabase(inputFile, jedis);
+
+            // Do the required statistics
+
+        } catch (Exception e) {
+            log.error("Error connecting to Redis: ", e);
         }
     }
 }
